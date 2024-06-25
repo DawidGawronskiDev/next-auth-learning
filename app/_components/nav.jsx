ad65a916b4167cd20025937ebee9dee39d609c36
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-06-25 16:41:49 +0100

    Update navigation

--- app/_components/nav.jsx
+++ app/_components/nav.jsx
@@ -1,7 +1,11 @@
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 import React from "react";
+import { options } from "../api/auth/[...nextAuth]/options";
 
-const Nav = () => {
+const Nav = async () => {
+  const session = await getServerSession(options);
+
   return (
     <header className="bg-gray-600 text-gray-100">
       <nav className="flex justify-between items-center w-full px-10 py-4">
@@ -12,6 +16,11 @@ const Nav = () => {
           <Link href="/clientmember">Client Member</Link>
           <Link href="/member">Member</Link>
           <Link href="/public">Public</Link>
+          {session ? (
+            <Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
+          ) : (
+            <Link href="/api/auth/signin">Login</Link>
+          )}
         </div>
       </nav>
     </header>
